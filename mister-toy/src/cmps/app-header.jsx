410ce0da42f1181da97617248571d1cf87de482a
Adi Ben Ami <adi.afriat@gmail.com>
2023-05-27 20:36:53 +0300

    added hamburger menu

--- mister-toy/src/cmps/app-header.jsx
+++ mister-toy/src/cmps/app-header.jsx
@@ -1,19 +1,26 @@
 import { Link, NavLink } from 'react-router-dom'
 import logo from '../assets/img/toy-logo.png'
+import { useState } from 'react'
+
 
 export function AppHeader() {
-    // const user = useSelector((storeState) => storeState.userModule.loggedinUser)
+    const [isMenuOpen, setIsMenuOpen] = useState(false)
 
+    function toggleMenu() {
+        setIsMenuOpen(prevIsMenuOpen => !prevIsMenuOpen)
+    }
 
+    const isOpen = (isMenuOpen) ? 'menu-open' : ''
     return(
         <header className="app-header full">
             <div className="nav-items-container">
                 <img className='toy-logo' src={logo} />
-                <nav className="main-nav">
-                    <NavLink to="/">Home</NavLink> |
-                    <NavLink to="/toy">Toys</NavLink> |
-                    <NavLink to="/about">About</NavLink> |
+                <nav className={isOpen}>
+                    <NavLink to="/">Home</NavLink> 
+                    <NavLink to="/toy">Toys</NavLink> 
+                    <NavLink to="/about">About</NavLink> 
                     <NavLink to="/dashboard">Dashboard</NavLink>
+                    <button className="hamburger-btn" onClick={()=> toggleMenu()}>☰</button>
                 </nav>
             </div>
         </header>
